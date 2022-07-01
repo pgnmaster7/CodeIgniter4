a683138a52437d93acfbefc31a486f43f2b8950e
pgnmaster7 <49881244+pgnmaster7@users.noreply.github.com>
2022-07-01 15:47:53 +0700

    Update welcome_message.php
    test

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -300,7 +300,7 @@
     <div class="copyrights">
 
         <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
-            open source licence.</p>
+            open source licence test.</p>
 
     </div>
 
